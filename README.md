1fad0fdb30ea4bff17bad9d0c630bd5cf9d6bd0f
DMGithubPublisher <github@dynamsoft.com>
2023-07-04 18:33:08 +0800

    update to DDN v2.0

---
+++ README.md
@@ -0,0 +1,34 @@
+# Dynamsoft Document Normalizer Samples - C++ Edition
+
+This repository contains multiple samples that demonstrate how to use the <a href="https://www.dynamsoft.com/document-normalizer/docs/core/introduction/?ver=latest" target="_blank">Dynamsoft Document Normalizer</a> C++ Edition.
+
+## Requirements
+
+- Operating systems:
+  - Windows: 7, 8, 10, 2003, 2008, 2008 R2, 2012 and above.
+  - Linux x64: Ubuntu 14.04.4+ LTS, Debian 8+, etc
+- Developing Tool
+  - Visual Studio 2012 or above
+  - G++ 5.4+  
+  - GCC 5.4+
+
+## Samples
+
+| Sample | Description |
+|---|---|
+| [HelloWorld\NormalizeAnImage](Samples\HelloWorld\NormalizeAnImage) | This sample demonstrates the simplest way to normalize an image and save the result as a new image. |
+| [HelloWorld\NormalizeMultipleImages](Samples\HelloWorld\NormalizeMultipleImages) | This sample demonstrates the simplest way to normalize image files in a directory and save the result as new images. |
+
+## License
+
+The library requires a license to work, you use the API InitLicense to initialize license key and activate the SDK.
+
+These samples use a <a href="https://www.dynamsoft.com/license-server/docs/about/terms.html?ver=latest&product=ddn&utm_source=samples&package=c_cpp#public-trial-license" target="_blank">free public trial license</a>. Note that network connection is required for this license to work.
+
+You can also request a 30-day trial license from <a href="https://www.dynamsoft.com/customer/license/trialLicense?product=ddn&utm_source=samples&package=c_cpp" target="_blank">Customer Portal</a>
+
+For more information, please refer to <a href="https://www.dynamsoft.com/license-server/docs/about/licensefaq.html">license FAQ</a>.
+
+## Contact Us
+
+<a href="https://www.dynamsoft.com/company/contact/">Contact Dynamsoft</a> if you have any questions.
---
+++ Samples/HelloWorld/NormalizeAnImage/NormalizeAnImage.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+#include<string>
+
+#include "../../../Include/DynamsoftCaptureVisionRouter.h"
+
+using namespace std;
+using namespace dynamsoft::cvr;
+using namespace dynamsoft::ddn;
+using namespace dynamsoft::license;
+using namespace dynamsoft::utility;
+
+// The following code only applies to Windows.
+#if defined(_WIN64) || defined(_WIN32)
+#ifdef _WIN64
+#pragma comment(lib, "../../../Lib/Windows/x64/DynamsoftCaptureVisionRouterx64.lib")
+#pragma comment(lib, "../../../Lib/Windows/x64/DynamsoftLicensex64.lib")
+#pragma comment(lib, "../../../Lib/Windows/x64/DynamsoftUtilityx64.lib")
+#else
+#pragma comment(lib, "../../../Lib/Windows/x86/DynamsoftCaptureVisionRouterx86.lib")
+#pragma comment(lib, "../../../Lib/Windows/x86/DynamsoftLicensex86.lib")
+#pragma comment(lib, "../../../Lib/Windows/x86/DynamsoftUtilityx86.lib")
+#endif
+#endif
+
+int main()
+{
+	int errorcode = 0;
+	char error[512];
+
+	// 1.Initialize license.
+	// You can request and extend a trial license from https://www.dynamsoft.com/customer/license/trialLicense?product=ddn&utm_source=samples
+	// The string 'DLS2eyJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSJ9' here is a free public trial license. Note that network connection is required for this license to work.
+	errorcode = CLicenseManager::InitLicense("DLS2eyJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSJ9", error, 512);
+
+	cout << "License initialization: " << errorcode << "," << error << endl;
+
+	// 2.Create an instance of CCaptureVisionRouter.
+	CCaptureVisionRouter *router = new CCaptureVisionRouter;
+
+	// 3.Normalize an image
+	string imageFile = "../../../Images/sample-image.png";
+	CCapturedResult* result = router->Capture(imageFile.c_str(), CPresetTemplate::PT_DETECT_AND_NORMALIZE_DOCUMENT);
+	
+	cout << "File: " << imageFile << endl;
+
+	if (result->GetErrorCode() != 0) {
+		cout << "Error: " << result->GetErrorCode() << "," << result->GetErrorString() << endl;
+	}
+
+	int count = result->GetCount();
+	cout << "Normalized " << count << " documents" << endl;
+	for (int i = 0; i < count; i++) {
+		const CCapturedResultItem* item = result->GetItem(i);
+
+		CapturedResultItemType type = item->GetType();
+		if (type == CapturedResultItemType::CRIT_NORMALIZED_IMAGE) {
+			const CNormalizedImageResultItem* normalizedImage = dynamic_cast<const CNormalizedImageResultItem*>(item);
+
+			string outPath = "normalizedResult_";
+			outPath += to_string(i) + ".png";
+
+			CImageManager manager;
+
+			// 4.Save normalized image to file.
+			errorcode = manager.SaveToFile(normalizedImage->GetImageData(), outPath.c_str());
+			if (errorcode == 0) {
+				cout << "Document " << i << " file: " << outPath << endl;
+			}
+		}
+	}
+
+	// 5. Release the allocated memory.
+	delete router, router = NULL;
+	delete result, result = NULL;
+
+	return 0;
+}
